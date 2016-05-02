a8614448fb490424b9841ad8e79aa0efe5a29d05
GroverTB <grover.jtb@gmail.com>
2016-05-02 18:00:54 -0500

    INIT

---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+import React, { requireNativeComponent, PropTypes, View } from 'react-native';
+var { addons: { PureRenderMixin }, NativeModules: { UIManager, CrosswalkWebViewManager: { JSNavigationScheme } } } = React;
+
+var WEBVIEW_REF = 'crosswalkWebView';
+
+var CrosswalkWebView = React.createClass({
+    mixins:    [PureRenderMixin],
+    statics:   { JSNavigationScheme },
+    propTypes: {
+        localhost:               PropTypes.bool.isRequired,
+        onNavigationStateChange: PropTypes.func,
+        url:                     PropTypes.string,
+        injectedJavascript:      PropTypes.string,
+        ...View.propTypes
+    },
+    getDefaultProps () {
+        return {
+            localhost: false
+        };
+    },
+    render () {
+        return (
+            <NativeCrosswalkWebView
+                { ...this.props }
+                onNavigationStateChange={ this.onNavigationStateChange }
+                ref={ WEBVIEW_REF }/>
+        );
+    },
+    getWebViewHandle () {
+        return React.findNodeHandle(this.refs[WEBVIEW_REF]);
+    },
+    onNavigationStateChange (event) {
+        var { onNavigationStateChange } = this.props;
+        if (onNavigationStateChange) {
+            onNavigationStateChange(event.nativeEvent);
+        }
+    },
+    goBack () {
+        UIManager.dispatchViewManagerCommand(
+            this.getWebViewHandle(),
+            UIManager.NativeCrosswalkWebView.Commands.goBack,
+            null
+        );
+    },
+    goForward () {
+        UIManager.dispatchViewManagerCommand(
+            this.getWebViewHandle(),
+            UIManager.NativeCrosswalkWebView.Commands.goForward,
+            null
+        );
+    },
+    reload () {
+        UIManager.dispatchViewManagerCommand(
+            this.getWebViewHandle(),
+            UIManager.NativeCrosswalkWebView.Commands.reload,
+            null
+        );
+    }
+});
+
+var NativeCrosswalkWebView = requireNativeComponent('CrosswalkWebView', CrosswalkWebView);
+
+export default CrosswalkWebView;
